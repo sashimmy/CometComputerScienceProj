a38a5b1bc3d5215e971e0e695c51beaf56343233
sashimmy <102709136+sashimmy@users.noreply.github.com>
2023-03-03 21:17:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('form');
+const taskList = document.querySelector('#task-list');
+
+form.addEventListener('submit', addTask);
+
+function addTask(event) {
+  event.preventDefault();
+
+  const taskName = document.querySelector('#task-name').value;
+  const dueDate = document.querySelector('#due-date').value;
+  const priority = document.querySelector('#priority').value;
+
+  const taskItem = document.createElement('li');
+  taskItem.innerHTML = `
+    <span>${taskName}</span>
+    <span>${dueDate}</span>
+    <span>${priority}</span>
+    <button>Delete</button>
+  `;
+
+  taskList.appendChild(taskItem);
+
+  form.reset();
+}
